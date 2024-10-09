a5b329fd5419371fdece6b9cae7effdf2614de9d
noxyuh <rafaelricardofilho12@gmail.com>
2024-10-09 20:05:33 -0300

    Corrige inicialização do Firebase e upload de imagens

--- app.py
+++ app.py
@@ -5,30 +5,31 @@ import firebase_admin
 from firebase_admin import credentials, storage
 from werkzeug.utils import secure_filename
 
-
-
 # Carrega as credenciais do Firebase a partir da variável de ambiente
 cred_json = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_JSON')
 
-# Converte o JSON em um dicionário
-cred_dict = json.loads(cred_json)
-
-# Inicializa o Firebase
-cred = credentials.Certificate('cred_dict')
-firebase_admin.initialize_app(cred, {
-    'storageBucket': 'detec-serpentes.appspot.com'  
-})
+if cred_json:
+    
+    cred_dict = json.loads(cred_json)
+    
+    
+    cred = credentials.Certificate(cred_dict)
+    firebase_admin.initialize_app(cred, {
+        'storageBucket': 'detec-serpentes.appspot.com'  
+    })
+else:
+    raise Exception('GOOGLE_APPLICATION_CREDENTIALS_JSON não foi encontrada nas variáveis de ambiente')
 
 api = Flask(__name__)
 
-# Extensões de arquivo permitidos
-ALLOWED_EXTENSIONS = set(['png', 'jpeg', 'jpg'])
+
+ALLOWED_EXTENSIONS = {'png', 'jpeg', 'jpg'}
 
 
 def allowedFile(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# upload de uma foto
+
 @api.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
@@ -40,18 +41,17 @@ def upload_file():
         return jsonify({'error': 'Nenhum arquivo selecionado'}), 400
     
     if not allowedFile(file.filename):
-        return jsonify({'error': 'Extesao de arquivo nao permitido'}), 400
+        return jsonify({'error': 'Extensão de arquivo não permitida'}), 400
     
-    # Segurança no nome do arquivo
     filename = secure_filename(file.filename)
     
-    # Upload para o Firebase Storage
+    
     bucket = storage.bucket()
     blob = bucket.blob(f'imagesUsers/{filename}')
-    blob.upload_from_file(file)  # Faz o upload do arquivo para o Firebase
-    blob.make_public()
+    blob.upload_from_file(file)  
+
     
-    return jsonify({'success': f'Arquivo {filename} foi recebido'}), 200
+    return jsonify({'success': f'Arquivo {filename} foi recebido', 'url': blob.public_url}), 200
 
 if __name__ == "__main__":
     api.run(debug=True)
